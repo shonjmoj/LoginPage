d34d3ca4763e435a1c67a50db4c6e68fda9eab0e
Elmehdi Ounejjar <elounejj@e1r5p15.1337.ma>
2022-03-07 11:48:12 +0100

    fixin the hover over the 'login with 42' and adding a red outline in case of submiting with empty fields

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,6 @@
-const navlink = document.getElementById('dropdownNavbarLink');
+const navlink = document.getElementById('dropdownNavbarLink')
 const login = document.getElementById('login');
+const signup = document.getElementById('signup');
 
 navlink?.addEventListener('click', e => {
 	e.preventDefault();
@@ -22,5 +23,27 @@ login?.addEventListener('click', e => {
 			passwd.classList.remove('outline-error');
 		}, 3000)
 
+	}
+})
+
+signup?.addEventListener('click', e => {
+	e.preventDefault();
+	const fName = <HTMLInputElement>document.getElementById('name');
+	const lastName = <HTMLInputElement>document.getElementById('lastname');
+	const signEmail = <HTMLInputElement>document.getElementById('signemail');
+	const signPasswd = <HTMLInputElement>document.getElementById('signpasswd');
+
+	if (!fName.value && !lastName.value && !signEmail.value && !signPasswd.value) {
+
+		fName.classList.add('outline-error')
+		lastName.classList.add('outline-error');
+		signEmail.classList.add('outline-error');
+		signPasswd.classList.add('outline-error');
+		setTimeout(() => {
+			fName.classList.remove('outline-error')
+			lastName.classList.remove('outline-error');
+			signEmail.classList.remove('outline-error');
+			signPasswd.classList.remove('outline-error');
+		}, 3000)
 	}
 })
